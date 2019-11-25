2cda95dd808c9744fd1ab57800580436737f2607
leo <>
2019-11-25 16:10:16 +0800

    控制器列表

--- src/hsa_app/lib/page/home/home_page.dart
+++ src/hsa_app/lib/page/home/home_page.dart
@@ -4,10 +4,12 @@ import 'package:hsa_app/api/api.dart';
 import 'package:hsa_app/components/shawdow_widget.dart';
 import 'package:hsa_app/components/wave_ball.dart';
 import 'package:hsa_app/model/banner_item.dart';
+import 'package:hsa_app/model/station.dart';
 import 'package:hsa_app/page/home/view/home_banner.dart';
 import 'package:hsa_app/page/station/station_page.dart';
 import 'package:hsa_app/theme/theme_gradient_background.dart';
 import 'package:hsa_app/components/public_tool.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,12 +18,27 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
 
+  // 每页分页数
+  int pageRowsMax = 10;
   // 广告条
   List<BannerItem> banners = [];
   // 省份列表
   List<String> provinces = [];
   // UI分节列表
-  List<String> sections = ['特别关注','全部电站']; 
+  List<String> sections = [
+    '特别关注',
+    '全部电站',
+  ];
+  // 刷新控制器列表
+  List<RefreshController> refreshList = [
+    RefreshController(initialRefresh: false),
+    RefreshController(initialRefresh: false),
+  ];
+  // 当前页码列表
+  List<int> pagesList = [
+    1,
+    1,
+  ];
 
   // 获取广告条
   void requestBanner() {
@@ -32,7 +49,6 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
       });
     }, (String msg){
       debugPrint(msg);
-      // showErrorHud(context, msg);
       progressShowError(msg);
     });
   }
@@ -44,11 +60,61 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
       setState(() {
         this.provinces = provinces;
         this.sections.addAll(provinces.map((name)=>name+'省').toList());
+        for (var _ in provinces) {
+          this.refreshList.add(RefreshController(initialRefresh: false));
+          this.pagesList.add(1);
+        }
       });
     }, (String msg){
       debugPrint(msg);
       progressShowError(msg);
     });
+  }
+
+  // 按省获取电站列表 加载首页
+  void reqeustStationListLoadFirst(int section, int page, String province,bool isFocus) {
+
+    API.stationsList((List<Stations> stations,int total){
+      setState(() {
+
+      });
+    }, (String msg){
+      debugPrint(msg);
+      progressShowError(msg);
+    },
+    // 页码
+    page:page,
+    rows:pageRowsMax,
+    );
+  }
+
+  // // 按省获取电站列表 加载更多
+  // void reqeustStationListLoadNext() {
+
+  //   API.stationsList((List<Stations> stations,int total){
+  //     setState(() {
+
+  //     });
+  //   }, (String msg){
+  //     debugPrint(msg);
+  //     progressShowError(msg);
+  //   });
+  // }
+
+   // 下拉刷新
+   void _onRefresh(RefreshController refreshController) async{
+    await Future.delayed(Duration(milliseconds: 1000));
+    refreshController.refreshCompleted();
+  }
+
+  // 上拉加载更多
+  void _onLoading(RefreshController refreshController) async{
+    await Future.delayed(Duration(milliseconds: 1000));
+    if(mounted)
+    setState(() {
+
+    });
+    refreshController.loadComplete();
   }
 
 
@@ -57,7 +123,6 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     super.initState();
     requestBanner();
     requestProvinces();
-    // ProgressHud.of(context).showLoading();
   }
 
   @override
@@ -101,12 +166,18 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     );
   }
 
+  // 电站列表
   Widget stationList() {
-    return Container(
+    return SmartRefresher(
+      enablePullDown: true,
+      enablePullUp: true,
+      // onRefresh: _onRefresh,
+      // onLoading: _onLoading,
       child: ListView.builder(
         itemCount: 10,
         itemBuilder: (_, int i) => stationTile(),
-      ),
+      ), 
+      // controller: _refreshController,
     );
   }
 
@@ -125,9 +196,9 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
                 height: 24,
                 width: 24,
                 child: CircleAvatar(
-                    radius: 12,
-                    backgroundImage:
-                        AssetImage('images/home/Home_protrait_icon.png')),
+                  radius: 12,
+                  backgroundImage:AssetImage('images/home/Home_protrait_icon.png'),
+                ),
               ),
               SizedBox(width: 10),
               Text('登云水电站',style: TextStyle(color: Colors.white, fontSize: 16)),
