4545633f225f98dd15db2938f0ae2375d1fbede6
leo <>
2019-12-06 15:50:02 +0800

    远程控制调节

--- src/hsa_app/lib/page/dialog/control_model_dialog.dart
+++ src/hsa_app/lib/page/dialog/control_model_dialog.dart
@@ -17,7 +17,7 @@ class ControlModelDialog extends Dialog {
               padding: EdgeInsets.symmetric(horizontal: 10),
               child: SizedBox(
                 width: double.infinity,
-                height: 152,
+                height: 280,
                 child: Container(
                   decoration: ShapeDecoration(
                     color: Color.fromRGBO(57, 137, 199, 1),
@@ -32,7 +32,7 @@ class ControlModelDialog extends Dialog {
                       mainAxisAlignment: MainAxisAlignment.start,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        //  有功调控
+                        // 手动
                         Container(
                           margin: EdgeInsets.symmetric(horizontal: 12),
                           child: SizedBox(
@@ -41,9 +41,9 @@ class ControlModelDialog extends Dialog {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: <Widget>[
-                                Text('有功调控',
+                                Text('手动',
                                     style: TextStyle(
-                                        color: Colors.white, fontSize: 16)),
+                                        color: Colors.white60, fontSize: 16)),
                                 SizedBox(
                                     height: 22,
                                     width: 22,
@@ -62,7 +62,7 @@ class ControlModelDialog extends Dialog {
                           color: Colors.white12,
                         ),
 
-                        // 无功调控
+                        // 自动
                         Container(
                           margin: EdgeInsets.symmetric(horizontal: 12),
                           child: SizedBox(
@@ -71,7 +71,85 @@ class ControlModelDialog extends Dialog {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               crossAxisAlignment: CrossAxisAlignment.center,
                               children: <Widget>[
-                                Text('无功调控',
+                                Text('自动',
+                                    style: TextStyle(
+                                        color: Colors.white60, fontSize: 16)),
+                                SizedBox(
+                                    height: 22,
+                                    width: 22,
+                                    child: Image.asset(
+                                        'images/runtime/Time_selected_icon.png')),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        // 分割线
+                        Container(
+                          width: double.infinity,
+                          margin: EdgeInsets.only(left: 12),
+                          height: 1,
+                          color: Colors.white12,
+                        ),
+
+                        // 智能
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 12),
+                          child: SizedBox(
+                            height: 43,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                RichText(
+                                  text: TextSpan(children: [
+                                    TextSpan(
+                                        text: '智能',
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 16)),
+                                    TextSpan(
+                                        text: ' (下位机智能控制屏旋钮处于智能模式时,方可开启)',
+                                        style: TextStyle(
+                                            color: Colors.white60, fontSize: 12)),
+                                  ]),
+                                ),
+                                SizedBox(
+                                    height: 22,
+                                    width: 22,
+                                    child: Image.asset(
+                                        'images/runtime/Time_selected_icon.png')),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        // 分割线
+                        Container(
+                          width: double.infinity,
+                          margin: EdgeInsets.only(left: 12),
+                          height: 1,
+                          color: Colors.white12,
+                        ),
+
+
+
+                        // // 分割线
+                        // Container(
+                        //   width: double.infinity,
+                        //   height: 1,
+                        //   color: Colors.white12,
+                        // ),
+
+                        // 远程控制
+                        Container(
+                          margin: EdgeInsets.only(right: 12,left: 40),
+                          child: SizedBox(
+                            height: 43,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text('远程控制',
                                     style: TextStyle(
                                         color: Colors.white, fontSize: 16)),
                                 SizedBox(
@@ -87,9 +165,40 @@ class ControlModelDialog extends Dialog {
                         // 分割线
                         Container(
                           width: double.infinity,
+                          margin: EdgeInsets.only(left: 40),
                           height: 1,
                           color: Colors.white12,
                         ),
+
+                        // 智能水位控制
+                        Container(
+                          margin: EdgeInsets.only(right: 12,left: 40),
+                          child: SizedBox(
+                            height: 43,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Text('智能水位控制',
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 16)),
+                                SizedBox(
+                                    height: 22,
+                                    width: 22,
+                                    child: Image.asset(
+                                        'images/runtime/Time_selected_icon.png')),
+                              ],
+                            ),
+                          ),
+                        ),
+
+                        // 分割线顶格
+                        Container(
+                          width: double.infinity,
+                          height: 1,
+                          color: Colors.white12,
+                        ),
+
 
                         // 底部扩展区
                         Expanded(
@@ -104,7 +213,7 @@ class ControlModelDialog extends Dialog {
                                   Expanded(
                                     flex: 1,
                                     child: GestureDetector(
-                                      onTap: (){
+                                      onTap: () {
                                         Navigator.of(context).pop();
                                       },
                                       child: Container(
