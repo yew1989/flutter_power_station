10826cdf98391ef33b0d444395024666ed80ac01
leo <>
2019-12-18 14:53:36 +0800

    更新友盟底库

--- src/hsa_app/lib/service/umeng_analytics.dart
+++ src/hsa_app/lib/service/umeng_analytics.dart
@@ -1,37 +1,43 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter_umeng_analytics/flutter_umeng_analytics.dart';
+import 'package:hsa_app/api/leancloud/leancloud_api.dart';
+import 'package:hsa_app/config/app_config.dart';
+import 'package:lcfarm_flutter_umeng/lcfarm_flutter_umeng.dart';
 
 class UMengAnalyticsService {
 
+  static String getChannelName() {
+    var env = AppConfig.getInstance().env;
+    if(env == LeanCloudEnv.dev) return '开发';
+    if(env == LeanCloudEnv.test) return '测试';
+    if(env == LeanCloudEnv.product) return '生产';
+    return '未知';
+  }
+
   static void init() {
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      UMengAnalytics.init('5df9720d4ca357120b000d92',
-      logEnable:true,
-      policy: Policy.BATCH, encrypt: true, reportCrash: true);
-    }
-    else if (defaultTargetPlatform == TargetPlatform.iOS) {
-      UMengAnalytics.init('5df9724c0cafb2eaed000202',
-      logEnable:true,
-      policy: Policy.BATCH, encrypt: true, reportCrash: true);
-    }  
+      LcfarmFlutterUmeng.init(
+        iOSAppKey: '5df9724c0cafb2eaed000202',
+        androidAppKey: '5df9720d4ca357120b000d92',
+        channel: getChannelName(),
+        logEnable: true,
+        encrypt: false);
   }
 
   static void enterPage(String name) {
     if(name == null) return;
     if(name.length == 0) return;
-    UMengAnalytics.beginPageView(name);
+    LcfarmFlutterUmeng.beginLogPageView(name);
   }
 
   static void exitPage(String name) {
     if(name == null) return;
     if(name.length == 0) return;
-    UMengAnalytics.endPageView(name);
+    LcfarmFlutterUmeng.endLogPageView(name);
   }
 
-  static void logEvent(String name) {
-    if(name == null) return;
-    if(name.length == 0) return;
-    UMengAnalytics.logEvent(name,label:name);
+  static void logEvent(String eventId,String label) {
+    if(eventId == null) return;
+    if(eventId.length == 0) return;
+    if(label == null) return;
+    if(label.length == 0) return;
+    LcfarmFlutterUmeng.event(eventId,label:label);
   }
 }
