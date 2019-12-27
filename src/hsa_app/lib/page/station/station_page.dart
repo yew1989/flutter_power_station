8d733a4eb73446a16a9a492a7bc75ee67d59d659
leo <>
2019-12-27 14:36:16 +0800

    代码整理

--- src/hsa_app/lib/page/station/station_page.dart
+++ src/hsa_app/lib/page/station/station_page.dart
@@ -1,8 +1,6 @@
-import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:hsa_app/components/smart_refresher_style.dart';
 import 'package:hsa_app/page/station/device/station_device_list.dart';
-import 'package:hsa_app/page/station/device/station_device_list_tile.dart';
 import 'package:hsa_app/page/station/station_big_pool.dart';
 import 'package:hsa_app/page/station/station_list_header.dart';
 import 'package:hsa_app/page/station/station_weather_widget.dart';
@@ -10,31 +8,29 @@ import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:hsa_app/api/api.dart';
 import 'package:hsa_app/config/app_config.dart';
 import 'package:hsa_app/model/station_info.dart';
-import 'package:hsa_app/page/runtime/runtime_page.dart';
 import 'package:hsa_app/page/framework/webview_page.dart';
 import 'package:hsa_app/service/umeng_analytics.dart';
 import 'package:hsa_app/theme/theme_gradient_background.dart';
 import 'package:hsa_app/components/public_tool.dart';
 import 'package:hsa_app/util/share.dart';
-import 'package:native_color/native_color.dart';
 import 'package:ovprogresshud/progresshud.dart';
 
 class StationPage extends StatefulWidget {
-
   final String title;
   final String stationId;
- 
+
   StationPage(this.title, this.stationId);
+
   @override
   _StationPageState createState() => _StationPageState();
 }
 
 class _StationPageState extends State<StationPage> {
-
   StationInfo stationInfo = StationInfo();
   String weather = '晴';
   List<String> openLive = [];
-  RefreshController refreshController = RefreshController(initialRefresh: false);
+  RefreshController refreshController =
+      RefreshController(initialRefresh: false);
 
   @override
   void initState() {
@@ -43,7 +39,7 @@ class _StationPageState extends State<StationPage> {
     super.initState();
   }
 
-    @override
+  @override
   void dispose() {
     Progresshud.dismiss();
     UMengAnalyticsService.exitPage('电站概要');
@@ -52,35 +48,30 @@ class _StationPageState extends State<StationPage> {
 
   // 请求电站概要
   void reqeustStationInfo() {
-
     Progresshud.showWithStatus('读取数据中...');
 
-    final stationId = widget?.stationId ?? '' ;
+    final stationId = widget?.stationId ?? '';
 
-    if(stationId.length == 0) {
+    if (stationId.length == 0) {
       Progresshud.showInfoWithStatus('获取电站信息失败');
       return;
     }
-    
-    API.stationInfo(stationId,(StationInfo station){
 
+    API.stationInfo(stationId, (StationInfo station) {
       Progresshud.dismiss();
       refreshController.refreshCompleted();
 
-      if(station == null) return;
+      if (station == null) return;
 
       setState(() {
-        this.stationInfo =  station;
-        if(station.openlive!= null) {
+        this.stationInfo = station;
+        if (station.openlive != null) {
           this.openLive = station.openlive;
         }
       });
-
-    },(String msg){
-
+    }, (String msg) {
       refreshController.refreshFailed();
       Progresshud.showInfoWithStatus('获取电站信息失败');
-
     });
   }
 
@@ -89,19 +80,26 @@ class _StationPageState extends State<StationPage> {
     final host = AppConfig.getInstance().remotePackage.hostWeb;
     final urlHistory = host + '/#/' + 'history';
     final auth = await ShareManager.instance.loadToken();
-    final deviceIdList = stationInfo.devices.map((device){
+    final deviceIdList = stationInfo.devices.map((device) {
       return device?.address ?? '';
     }).toList();
     final terminalsString = deviceIdList.join(',');
     final titleString = stationInfo?.name ?? '';
-    final lastUrl =  urlHistory + '?auth=' + auth + '&address=' + terminalsString + '&title=' + Uri.encodeComponent(titleString);
-    pushToPage(context, WebViewPage('', lastUrl,noNavBar:true,description:'历史曲线'));
+    final lastUrl = urlHistory +
+        '?auth=' +
+        auth +
+        '&address=' +
+        terminalsString +
+        '&title=' +
+        Uri.encodeComponent(titleString);
+    pushToPage(
+        context, WebViewPage('', lastUrl, noNavBar: true, description: '历史曲线'));
   }
 
   // 同步天气数据
   void syncWeaher(String weather) async {
-   await Future.delayed(Duration(milliseconds: 500));
-   setState(() {
+    await Future.delayed(Duration(milliseconds: 500));
+    setState(() {
       this.weather = weather;
     });
   }
@@ -109,47 +107,54 @@ class _StationPageState extends State<StationPage> {
   @override
   Widget build(BuildContext context) {
     return ThemeGradientBackground(
-      child: Stack(
-        children:[
-
-          stationInfo?.geo == null ? Container() : 
-          StationWeatherWidget(geo: stationInfo.geo,onWeahterResponse: (weather) => syncWeaher(weather)),
-
-          Scaffold(
+      child: Stack(children: [
+        stationInfo?.geo == null
+            ? Container()
+            : StationWeatherWidget(
+                geo: stationInfo.geo,
+                onWeahterResponse: (weather) => syncWeaher(weather)),
+        Scaffold(
           backgroundColor: Colors.transparent,
           appBar: AppBar(
             backgroundColor: Colors.transparent,
             elevation: 0,
             centerTitle: true,
-            title: Text(widget.title ?? '',style: TextStyle(color: Colors.white,fontWeight: FontWeight.normal,fontSize: 20)),
+            title: Text(widget.title ?? '',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.normal,
+                    fontSize: 20)),
             actions: <Widget>[
               GestureDetector(
-                onTap: stationInfo.devices == null ? null : (){
-                  onTapPushToHistory();
-                },
-                child: Center(child: Text('历史曲线',style:TextStyle(color: Colors.white,fontSize: 16)))),
+                  onTap: stationInfo.devices == null
+                      ? null
+                      : () {
+                          onTapPushToHistory();
+                        },
+                  child: Center(
+                      child: Text('历史曲线',
+                          style:
+                              TextStyle(color: Colors.white, fontSize: 16)))),
               SizedBox(width: 20),
             ],
           ),
           body: Container(
             child: SmartRefresher(
-                header: appRefreshHeader(),
-                enablePullDown: true,
-                onRefresh: reqeustStationInfo,
-                controller: refreshController,
+              header: appRefreshHeader(),
+              enablePullDown: true,
+              onRefresh: reqeustStationInfo,
+              controller: refreshController,
               child: ListView(
                 children: <Widget>[
                   StationBigPool(stationInfo),
-                  StationListHeader(weather,openLive,stationInfo.name),
+                  StationListHeader(weather, openLive, stationInfo.name),
                   StationDeviceList(stationInfo),
                 ],
               ),
             ),
           ),
         ),
-        
-        ]
-      ),
+      ]),
     );
   }
 }
