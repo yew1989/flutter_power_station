095f92c53ef4bd1893d3c8e05f174dbea95a20ae
leo <>
2020-01-07 08:23:44 +0800

    注册码

--- src/hsa_app/lib/app/app.dart
+++ src/hsa_app/lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hsa_app/api/api.dart';
 import 'package:hsa_app/config/app_config.dart';
 import 'package:hsa_app/page/welcome/welcome_page.dart';
+import 'package:syncfusion_flutter_core/core.dart';
 
 class MyApp extends StatefulWidget {
   @override
@@ -14,6 +15,7 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     AppConfig.initConfig();
     API.touchNetWork();
+    SyncfusionLicense.registerLicense("NT8mJyc2IWhiZH1nfWN9YGpoYmF8YGJ8ampqanNiYmlmamlmanMDHmhia2NnZWNmYGJqYBNiZWB9MDw+");
     super.initState();
   }
 
