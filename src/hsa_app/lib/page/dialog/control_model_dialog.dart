58f34086d20fadd18c4e9097ad95e42130b02646
leo <gitea@fake.local>
2020-04-14 15:14:49 +0800

    解决bug,修改终端控制方案后,终端方案没有立即变化问题

--- src/hsa_app/lib/page/dialog/control_model_dialog.dart
+++ src/hsa_app/lib/page/dialog/control_model_dialog.dart
@@ -5,11 +5,9 @@ class ControlModelDialogWidget extends StatefulWidget {
 
   final DeviceTerminal deviceTerminal;
 
-  final String currentStatusStr;
-
   final void Function(ControlModelCurrentStatus status,String currentStatusStr) onChoose;
 
-  const ControlModelDialogWidget({Key key, this.deviceTerminal, this.currentStatusStr, this.onChoose}) : super(key: key);
+  const ControlModelDialogWidget({Key key, this.deviceTerminal, this.onChoose}) : super(key: key);
 
   @override
   _ControlModelDialogWidgetState createState() => _ControlModelDialogWidgetState();
@@ -18,16 +16,15 @@ class ControlModelDialogWidget extends StatefulWidget {
 
 class _ControlModelDialogWidgetState extends State<ControlModelDialogWidget> {
   
-  ControlModelCurrentStatus currentStatus;
   DeviceTerminal deviceTerminal = DeviceTerminal();
   String currentStatusStr = '';
+  ControlModelCurrentStatus currentStatus;
 
   @override
   void initState() {
     deviceTerminal = widget?.deviceTerminal;
-    currentStatus = getCurrentStatus();
+    getCurrentStatus();
     super.initState();
-
   }
 
   @override
@@ -43,22 +40,44 @@ class _ControlModelDialogWidgetState extends State<ControlModelDialogWidget> {
   
 
   //当前模式
-  ControlModelCurrentStatus getCurrentStatus(){
-    if(deviceTerminal.controlType != null){
+  void getCurrentStatus(){
+
+    // 默认从基础数据里面拿
+    if(deviceTerminal.controlType != null) {
       if(deviceTerminal.controlType.compareTo('智能') == 0){
         this.currentStatusStr = '智        能';
-        return deviceTerminal.isAllowRemoteControl ? ControlModelCurrentStatus.remoteOn : ControlModelCurrentStatus.remoteOff;
+        this.currentStatus =  deviceTerminal.isAllowRemoteControl ? ControlModelCurrentStatus.remoteOn : ControlModelCurrentStatus.remoteOff;
       }else if(deviceTerminal.controlType.compareTo('手动') == 0){
         this.currentStatusStr = '手        动';
-        return ControlModelCurrentStatus.manual;
+        this.currentStatus = ControlModelCurrentStatus.manual;
       }else if(deviceTerminal.controlType.compareTo('自动') == 0){
         this.currentStatusStr = '自        动';
-        return ControlModelCurrentStatus.auto;
+        this.currentStatus = ControlModelCurrentStatus.auto;
       }else{
-        return ControlModelCurrentStatus.unknow;
+        this.currentStatusStr = '未        知';
+        this.currentStatus = ControlModelCurrentStatus.unknow;
       }
-    }else{
-      return ControlModelCurrentStatus.unknow;
+    }
+    else {
+      this.currentStatusStr = '未        知';
+      this.currentStatus = ControlModelCurrentStatus.unknow;
+    }
+    
+    // 如果运行缓存有数据 从运行时参数里面取
+    if(deviceTerminal.nearestRunningData != null) {
+      if(deviceTerminal.nearestRunningData.controlType.compareTo('智能') == 0){
+        this.currentStatusStr = '智        能';
+        this.currentStatus =  deviceTerminal.nearestRunningData.isAllowRemoteControl ? ControlModelCurrentStatus.remoteOn : ControlModelCurrentStatus.remoteOff;
+      }else if(deviceTerminal.nearestRunningData.controlType.compareTo('手动') == 0){
+        this.currentStatusStr = '手        动';
+        this.currentStatus = ControlModelCurrentStatus.manual;
+      }else if(deviceTerminal.nearestRunningData.controlType.compareTo('自动') == 0){
+        this.currentStatusStr = '自        动';
+        this.currentStatus = ControlModelCurrentStatus.auto;
+      }else{
+        this.currentStatusStr = '未        知';
+        this.currentStatus = ControlModelCurrentStatus.unknow;
+      }
     }
   }
 }
@@ -140,8 +159,7 @@ class ControlModelDialog extends Dialog {
 
   Widget modelTile(BuildContext context, String string, bool enable,
       ControlModelCurrentStatus status , String currentStatusStr) {
-    var isRemoteMode = status == ControlModelCurrentStatus.remoteOff ||
-        status == ControlModelCurrentStatus.remoteOn;
+    var isRemoteMode = (status == ControlModelCurrentStatus.remoteOff) || (status == ControlModelCurrentStatus.remoteOn);
    
     return Container(
       margin: EdgeInsets.only(right: 12, left: isRemoteMode ? 40 : 12),
@@ -161,9 +179,7 @@ class ControlModelDialog extends Dialog {
                   SizedBox(
                       height: 22,
                       width: 22,
-                      child: status == currentStatus
-                          ? Image.asset('images/runtime/Time_selected_icon.png')
-                          : null),
+                      child: status == currentStatus ? Image.asset('images/runtime/Time_selected_icon.png') : null),
                 ],
               ),
             ),
@@ -255,18 +271,15 @@ class ControlModelDialog extends Dialog {
                                               fontSize: isIphone5s ? 10 : 12)),
                                     ]),
                                   ),
-                                  // SizedBox(height: 22, width: 22),
                                 ],
                               ),
                             ),
                           ),
 
                           divider(12),
-                          modelTile(context, '远程控制', true,
-                              ControlModelCurrentStatus.remoteOn,this.currentStatusStr),
+                          modelTile(context, '远程控制', true,ControlModelCurrentStatus.remoteOn,this.currentStatusStr),
                           divider(40),
-                          modelTile(context, '${deviceTerminal.intelligentControlScheme}', true,
-                              ControlModelCurrentStatus.remoteOff,this.currentStatusStr),
+                          modelTile(context, '${deviceTerminal.intelligentControlScheme}', true,ControlModelCurrentStatus.remoteOff,this.currentStatusStr),
 
                           // 分割线顶格
                           Container(
