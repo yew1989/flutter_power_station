049083145886b893045b7c7447b38c0ca8b4dbee
zoujichao <>
2020-05-08 09:26:42 +0800

    删除废弃代码

--- src/hsa_app/lib/components/data_picker.dart
+++ src/hsa_app/lib/components/data_picker.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_cupertino_data_picker/flutter_cupertino_data_picker.dart';
+//import 'package:flutter_cupertino_data_picker/flutter_cupertino_data_picker.dart';
 import 'package:hsa_app/page/dialog/custom_dialog.dart';
 
 typedef DataPickerOnConfirmCallback(num item);
 
-void showDataPicker(BuildContext context,String title,List<String> datas,DataPickerOnConfirmCallback onConfirm) {
-       final bool showTitleActions = true;
-       DataPicker.showDatePicker(context,
-         showTitleActions: showTitleActions,
-         locale: 'zh',
-         datas: datas ?? [],
-         title: title ??'',
-         onConfirm:(data){
-           if(onConfirm != null) onConfirm(data);
-         }, 
-       );
-  }
+// void showDataPicker(BuildContext context,String title,List<String> datas,DataPickerOnConfirmCallback onConfirm) {
+//        final bool showTitleActions = true;
+//        DataPicker.showDatePicker(context,
+//          showTitleActions: showTitleActions,
+//          locale: 'zh',
+//          datas: datas ?? [],
+//          title: title ??'',
+//          onConfirm:(data){
+//            if(onConfirm != null) onConfirm(data);
+//          }, 
+//        );
+//   }
 
 void showNumberPicker(BuildContext context,DataPickerOnConfirmCallback onConfirm,{String title,int current,int max,int decimal}){
   showDialog(
