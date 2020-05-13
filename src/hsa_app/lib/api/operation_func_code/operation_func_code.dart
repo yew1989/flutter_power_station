83d15171c8c6fe611cddacb7d900d496c3e8745c
leo <gitea@fake.local>
2020-05-13 09:14:28 +0800

    权限管理功能

--- src/hsa_app/lib/api/operation_func_code/operation_func_code.dart
+++ src/hsa_app/lib/api/operation_func_code/operation_func_code.dart
@@ -2,17 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:hsa_app/api/api.dart';
 import 'package:hsa_app/api/api_helper.dart';
 import 'package:hsa_app/api/operation_func_code/operation_func_code_data.dart';
+import 'package:hsa_app/api/operation_func_code/operation_manager.dart';
 
 class OperationFuncCode {
-
+  
   static void getOperationFuncCode({@required String accountName}) {
     if(accountName == null) return;
     final path = API.baseHost + '/v1/OperationFuncCode/RelationAccount/' + '$accountName';
     HttpHelper.httpGET(path, null, (map,_){
-
       var resp = OperationFuncCodeResp.fromJson(map);
-      // debugPrint(resp.toString());
-      
+      if(resp.code != 200) return;
+      final data = resp.data;
+      OperationManager.getInstance().data = data;
     }, null);
   }
 }
