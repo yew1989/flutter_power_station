694614d4350187342d4bf9448905e38aa20d08d0
zoujichao <>
2020-05-15 14:57:02 +0800

    更换预估收益动画。改为随机量

--- src/hsa_app/lib/page/station/station_profit_widget.dart
+++ src/hsa_app/lib/page/station/station_profit_widget.dart
@@ -1,8 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hsa_app/config/app_config.dart';
 import 'package:hsa_app/config/app_theme.dart';
 import 'package:hsa_app/event/app_event.dart';
 import 'package:hsa_app/event/event_bird.dart';
+import 'package:supernova_flutter_ui_toolkit/keyframes.dart';
 
 class StationProfitWidget extends StatefulWidget {
 
@@ -18,6 +21,7 @@ class _StationProfitWidgetState extends State<StationProfitWidget> with TickerPr
   
   AnimationController controller;
   Animation<double> animation;
+  
 
   // 防止内存泄漏 当等于0时才触发动画
   var canPlayAnimationOnZero = 1;
@@ -28,12 +32,30 @@ class _StationProfitWidgetState extends State<StationProfitWidget> with TickerPr
     final profit    = widget?.profit[1] ?? 0.0;
     
     if(canPlayAnimationOnZero <= 0  && mounted ) {
+
       controller?.dispose();
       controller = AnimationController(duration: Duration(seconds:AppConfig.getInstance().stationPageAnimationDuration), vsync: this);
-      CurvedAnimation curvedAnimation = CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn);
-      animation = Tween<double>(begin: oldProfit, end: profit).animate(curvedAnimation);
+      //CurvedAnimation curvedAnimation = CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn);
+      //animation = Tween<double>(begin: oldProfit, end: profit).animate(curvedAnimation);
+      //3等分间隔
+
+      num p1 = oldProfit+(profit-oldProfit)*(Random().nextInt(50)+75)/300;
+      num p2 = oldProfit+(profit-oldProfit)*(Random().nextInt(50)+175)/300;
+
+      animation = Interpolation(
+        keyframes: [
+          Keyframe<double>(fraction: 0, value: oldProfit),
+          Keyframe<double>(fraction: 1/12, value: p1),
+          Keyframe<double>(fraction: 4/12, value: p1),
+          Keyframe<double>(fraction: 5/12, value: p2),
+          Keyframe<double>(fraction: 8/12, value: p2),
+          Keyframe<double>(fraction: 9/12, value: profit),
+          Keyframe<double>(fraction: 1, value: profit),]
+          
+      ).animate(controller);
       controller.forward();
       canPlayAnimationOnZero = 0 ;
+      //Random().nextInt(50);
     }
     canPlayAnimationOnZero --;
   }
